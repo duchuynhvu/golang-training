1711749f76fe7eb38f247b7a8ed899c7b54da85a
Huynh Vu <vdhuynh@tma.com.vn>
2019-07-22 16:18:42 +0700

    - part 1: topic about csvs, flags, opening files, strings packages

--- src/quiz/quiz.go
+++ src/quiz/quiz.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"os"
 	"encoding/csv"
+	"strings"
 )
 
 func main() {
@@ -15,14 +16,35 @@ func main() {
 	if err != nil {
 		exit(fmt.Sprintf("Failed to open the CSV file: %s", *csvFilename))
 	}
-
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
 	if err != nil {
 		exit(fmt.Sprintf("Failed to parse the provided CSV file."))
 	}
-	
+	problems := parseLines(lines)
 
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("Your scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem {
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
 }
 
 type problem struct {
